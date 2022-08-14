0eae4bd82d7478e8224f040a055b7384e8c2b149
Cherofme <denisderivedmed7@gmail.com>
2022-08-14 14:32:45 +0300

    D-09 complete

---
+++ DZ-09/DZ-09.py
@@ -0,0 +1,16 @@
+# from math_func import (square and rectangle)
+from math_func import square as sq
+from math_func import rectangle as rec
+
+#Then assign square 1-st and 2-nd
+s_1 = sq(10)
+s_2 = rec(4, 5)
+# Just sub 2 result
+result = s_1 - s_2
+
+if result > 0:
+    print('s1 > s2')
+elif result < 0:
+    print('s1 < s2')
+else:
+    print('s1 = s2')
---
+++ DZ-09/math_func.py
@@ -0,0 +1,11 @@
+from math import pi
+
+def square(diametr):
+    s = (diametr**2)/2
+    return s
+def rectangle(a,b):
+    s = (a * b)
+    return s
+def circle(radius):
+    s = pi * (radius**2)
+    return s
